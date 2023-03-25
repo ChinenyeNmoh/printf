95cc4d379d309251c800f328905572459a72f44b
agun36 <aguntoroti10@gmail.com>
2023-03-25 05:42:13 -0700

    printf created by partner B

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,5 @@
-#include <unistd.h>
 #include "main.h"
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,22 +8,58 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-
-int _putchar(char c)
+int _putchar(int c)
 {
-	return (write(1, &c, 1));
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ * Return: void
+ */
+
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
 }
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
  */
-int _puts(char *str)
-{
-	register short i;
 
-	for (i = 0; str[i]; i++)
-		_putchar(str[i]);
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
 	return (i);
+}
+
+/**
+ * _isdigit - checks if an int is a digit
+ * @c: int
+ * Return: return 1 if digit 0 if false
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
 }
